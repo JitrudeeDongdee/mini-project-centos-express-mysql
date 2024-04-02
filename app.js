d6225dac80a88a39cbb47a7b879ca1bc788fa20d
cookie cent-os <jitrudee9723@gmail.com>
2024-04-03 02:35:59 +0700

    add app

---
+++ app.js
@@ -0,0 +1,21 @@
+// เรียกใช้ Express.js
+const express = require('express');
+
+// สร้างแอปพลิเคชัน Express
+const app = express();
+
+// กำหนดพอร์ตที่แอปพลิเคชันจะรับคำขอ
+const port = 3000;
+
+// เส้นทางหลัก
+app.get('/', (req, res) => {
+  res.send('สวัสดีจากแอปพลิเคชัน Express!');
+});
+
+const mainroutes = require('./routse/netflixShowRoute');
+app.use('/api',mainroutes);
+
+// เริ่มต้นแอปพลิเคชันโดยที่มันจะฟังคำขอที่พอร์ตที่กำหนด
+app.listen(port, () => {
+  console.log(`แอปพลิเคชันกำลังทำงานที่ http://localhost:${port}`);
+});
